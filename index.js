4d8e8cc6babd5f0748abdf8e674128d7fc6f2a03
ruchit3601 <ruchitchudasama123@gmail.com>
2021-06-29 10:25:03 +0530

    add contenteditable attribute dynamically

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let globalStore = [];
 const generateNewCard = (taskData)=>`<div class="col-md-6 col-lg-4">
 <div class="card ">
   <div class="card-header d-flex justify-content-end gap-2">
-    <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+    <button type="button"  class="btn btn-outline-success" id=${taskData.id} onclick="editCard.apply(this,arguments)"><i class="fas fa-pencil-alt" id=${taskData.id} onclick="editCard.apply(this,arguments)"></i></button>
     <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"><i class="fas fa-trash-alt"id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i></button>
 
   </div>
@@ -77,6 +77,33 @@ const deleteCard = (event) =>{
   }
   //taskContainer.removeChild(document.getElementById(targetID));
 };
+
+const editCard = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
+
+  let parentElement;
+
+  if(tagname==="BUTTON"){
+    parentElemen = event.target.parentNode.parentNode;
+  }
+  else{
+    parentElement = event.target.parentNode.parentNode.parentNode;
+
+  }
+  let taskTitle = parentElement.childNodes[5].childNodes[1];
+  let taskDescription = parentElement.childNodes[5].childNodes[3];
+  let taskType = parentElement.childNodes[5].childNodes[5];
+  let submitButton = parentElement.childNodes[7].childNodes[1];
+
+  
+  taskTitle.setAttribute("contenteditable","true");
+  taskDescription.setAttribute("contenteditable","true");
+  taskType.setAttribute("contenteditable","true");
+  submitButton.innerHTML = "Save Changes";
+};
+
 //issues
 //page refresh will cause the data to be deleted -> local Storage->5MB[solved]
 
